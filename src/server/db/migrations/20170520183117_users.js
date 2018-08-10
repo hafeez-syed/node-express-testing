30165969dd7f4e2b74aab36ebe4ec9aa002f5c05
Hafeez <feeziman007@hotmail.com>
2018-08-09 21:59:30 -0400

    debug version updated to 3.1.0

---
+++ src/server/db/migrations/20170520183117_users.js
@@ -0,0 +1,14 @@
+/*jshint node:true */
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('users', (table) => {
+		console.log(table);
+		table.increments();
+		table.string('username').unique().notNullable();
+		table.string('email').unique().notNullable();
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('users');
+};
